212de6c74022ab003c16e694a1f32509a2d26796
Jihana <wldnjs7618@naver.com>
2023-04-01 14:47:15 +0900

    page, components setting

--- basic/src/index.js
+++ basic/src/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import AllProducts from '../pages/AllProducts';
+import Home from '../pages/Home';
+import ProductsDetail from '../pages/ProductDetail';
+import NewProduct from '../pages/NewProduct';
+import MyCart from '../pages/MyCart';
+import NotFound from '../pages/NotFound';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound/>,
+    children: [
+      {index: true, path: '/', element: <Home/>},
+      {path: '/products', element: <AllProducts/>},
+      {path: '/products/new', element: <NewProduct/>},
+      {path: '/products/:id', element: <ProductsDetail/>},
+      {path: '/carts', element: <MyCart/>}
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
