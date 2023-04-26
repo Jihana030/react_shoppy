e38ac9deb8c3b6ec6514c20d098c8c46a56714d2
Jihana <wldnjs7618@naver.com>
2023-04-26 15:18:10 +0900

    authcontent

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,22 +5,10 @@ import {BsFillPencilFill} from 'react-icons/bs';
 import {login, logout, onUserStateChange} from '../api/firebase';
 import User from "./User";
 import Button from "./ui/Button";
+import { useAuthContext } from "./context/AuthContent";
 
 export default function Navbar(){
-    const [user, setUser] = useState();
-
-    useEffect(()=>{
-        onUserStateChange((user)=>{
-            console.log(user);
-            setUser(user);
-        });
-    }, []);
-    // const handleLogin = () => {
-    //     login();
-    // }
-    // const handleLogout = () => {
-    //     logout();
-    // }
+    const {user, login, logout} = useAuthContext();
     return (
         <header className="flex justify-between border-b border-gray-300 p-2">
             <Link to='/' className='flex items-center text-4xl text-brand'>
@@ -29,7 +17,7 @@ export default function Navbar(){
             </Link>
             <nav className="flex items-center gap-4 font-semibold">
                 <Link to='/products'>Products</Link>
-                <Link to='/carts'>Carts</Link>
+                {user && <Link to='/carts'>Carts</Link>}
                 {user && user.isAdmins && (
                     <Link to='/products/new' className="text-2xl">
                         <BsFillPencilFill />
