5d881ea9a575e838aa489cacdaff37f5c4711de1
Jihana <wldnjs7618@naver.com>
2023-04-26 16:38:37 +0900

    new product

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { useAuthContext } from "../components/context/AuthContent";
 import { Navigate } from "react-router-dom";
 
-export default function ProtectedRoute({children, requirAdmin}){
+export default function ProtectedRoute({children, requireAdmin}){
     const {user} = useAuthContext();
-    if(!user || (requirAdmin && !user.isAdmin)){
+    if(!user || (requireAdmin && !user.isAdmin)){
         return <Navigate to="/" replace />
     }
 
